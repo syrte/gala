30281f54eb81de758af8cd0d7c3e4afd40d5fcb1
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-01-25 23:38:27 -0500

    added todo

--- gary/dynamics/core.py
+++ gary/dynamics/core.py
@@ -107,9 +107,6 @@ def classify_orbit(orbit=None, **kwargs):
     if orbit is None:
         orbit = Orbit(**kwargs)
 
-    # cartesian representation of orbit
-    # cart_orbit = orbit.represent_as(coord.CartesianRepresentation)
-
     # get angular momenta
     Ls = angular_momentum(orbit)
 
@@ -125,7 +122,6 @@ def classify_orbit(orbit=None, **kwargs):
         single_orbit = True
 
     for ii in range(3):
-        # TODO: I stopped here, need to figure out how to do this with the new shape of L
         cnd = (np.sign(L0[ii]) != np.sign(Ls[ii,1:])) | \
               (np.abs(Ls[ii,1:].value) < 1E-13)
 
@@ -138,36 +134,42 @@ def classify_orbit(orbit=None, **kwargs):
     else:
         return loop
 
-def align_circulation_with_z(w, loop_bit):
+def align_circulation_with_z(orbit=None, circulation=None, **kwargs):
     """
     If the input orbit is a tube orbit, this function aligns the circulation
     axis with the z axis.
 
     Parameters
     ----------
-    w : array_like
-        Array of phase-space positions. Accepts 2D or 3D arrays. If 2D, assumes
-        this is a single orbit so that `loop_bit` should be a 1D array. If 3D, assumes
-        that this is a collection of orbits, where `axis=0` is the time axis, and
-        `axis=1` are the different orbits.
-    loop_bit : array_like
+    orbit : :class:`~gary.dynamics.Orbit`
+        Orbit instance.
+    circulation : array_like (optional)
         Array of bits that specify the axis about which the orbit circulates.
-        See the documentation for ~`gary.dynamics.classify_orbit()`.
+        See the documentation for `~gary.dynamics.classify_orbit`. If not
+        provided, will run this function on the input orbit(s).
+    **keyword_arguments
+        Instead of passing in an `~gary.dynamics.Orbit`, it is also possible to pass in
+        a set of keyword arguments that fully specifies an `~gary.dynamics.Orbit` instance.
+        For example, you may pass in ``pos=``, ``vel=``, and (optionally) a representation
+        class. Any of the arguments accepted by the `~gary.dynamics.Orbit` initializer are
+        also accepted here.
 
     Returns
     -------
-    new_w : :class:`~numpy.ndarray`
-        A copy of the input array with circulation aligned with the z axis.
+    new_orbit : :class:`~gary.dynamics.Orbit`
+        A copy of the input `~gary.dynamics.Orbit` with the circulation
+        aligned with the z axis.
     """
 
-    if (w.ndim-1) != loop_bit.ndim:
-        raise ValueError("Shape mismatch - input orbit array should have 1 more dimension "
-                         "than the input loop bit.")
+    if orbit is None:
+        orbit = Orbit(**kwargs)
 
-    orig_shape = w.shape
-    if loop_bit.ndim == 1:
-        loop_bit = np.atleast_2d(loop_bit)
-        w = w[:,np.newaxis]
+    if circulation is None:
+        circulation = classify_orbit(orbit)
+
+    # TODO: stopped here because need to figure out better way to handle 1 vs many orbits
+    if circulation.ndim == 1:
+        pass
 
     new_w = w.copy()
     for ix in range(len(loop_bit)):
